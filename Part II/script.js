f182cb1e2fd3ba5c219cf1a564d1c031331acf63
Andres Montoya Angulo <jmontoyaan@unsa.edu.pe>
2022-01-11 12:48:59 -0500

    Me Gusta

--- Part II/script.js
+++ Part II/script.js
@@ -1,20 +1,8 @@
-let i = 9;
-let contador1 = document.querySelector("#contador1");
-function likes1(){
-    i++;
-    contador1.innerText = i + " " + "like(s)";
-}
-
-let j = 12;
-let contador2 = document.querySelector("#contador2");
-function likes2(){
-    j++;
-    contador2.innerText = j + " " + "like(s)";
-}
-
-let k = 9;
-let contador3 = document.querySelector("#contador3");
-function likes3(){
-    k++;
-    contador3.innerText = k + " " + "like(s)";
+function likes(element){
+    let contador = element.previousElementSibling;
+    let num = contador.innerText;
+    num = num.split("");
+    num = (Number(num[0])*10) + Number(num[1]);
+    num ++;
+    contador.innerText = num + " like(s)";
 }
